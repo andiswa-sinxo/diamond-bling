2459e9d1df5cb9ced66ae2ccd330a5a658de093c
andiswa-sinxo <andiswasinxo@gmail.com>
2023-02-02 15:38:41 +0200

    store

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3017;
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+    res.render('cart');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`)
+  });
---
+++ js/src.js
@@ -0,0 +1,9 @@
+module.exports = function shopping_cart() {
+
+    
+
+
+    return {
+
+    }
+}
